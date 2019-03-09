70cb3a9a7c9e9dd5919e7d1c0850ad4f55687909
student14 <student14@student14s-macbook-air.local>
2019-03-09 15:40:15 -0600

    Add tap and swipe gestures

--- Random Color/ViewController.swift
+++ Random Color/ViewController.swift
@@ -29,25 +29,45 @@ class ViewController: UIViewController {
     var blue = 255
     
     @IBOutlet weak var colorButton: UIButton!
+    @IBOutlet var tapGestureRecogniger: UITapGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Create tap gesgure recognizer and add it to the view
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        view.addGestureRecognizer(tap)
+        
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture))
+        view.addGestureRecognizer(swipe)
+        
         //set background color to white
         view.backgroundColor = UIColor(red: red, green: green, blue: blue)
+        
     } // end viewDidLoad()
     
-    // Displays the RBG values when display screen is pressed
+    // Displays the RGB values when display screen is pressed
     @IBAction func colorButtonClicked(_ sender: UIButton) {
+        
         let hexValue = String(format:"%02X", Int(red)) + String(format:"%02X", Int(green)) + String(format:"%02X", Int(blue))
         colorButton.setTitle("RGB (\(red),\(green),\(blue))\nHEX #\(hexValue)", for: .normal)
         colorButton.titleLabel?.textAlignment = NSTextAlignment.center
+    
     } // end colorButtonClicked()
     
-    //creates random RGB numbers and use them to set a new background color when device is shaken
+    // Displays the RGB and Hex values when user swipes the screen
+    @IBAction func handleSwipeGesture(_ sender: UISwipeGestureRecognizer) {
+        
+        let hexValue = String(format:"%02X", Int(red)) + String(format:"%02X", Int(green)) + String(format:"%02X", Int(blue))
+        colorButton.setTitle("RGB (\(red),\(green),\(blue))\nHEX #\(hexValue)", for: .normal)
+        colorButton.titleLabel?.textAlignment = NSTextAlignment.center
+
+    } // end handleSwipeGesture()
+    
+    // Creates random RGB numbers and use them to set a new background color when device is shaken
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         
-        //enable detection of shake motion
+        // Enable detection of shake motion
         if motion == .motionShake {
             
             //create random RGB numbers
@@ -55,13 +75,32 @@ class ViewController: UIViewController {
             green = Int.random(in: 0...255)
             blue = Int.random(in: 0...255)
             
-            //set background color with UIColor object
+            // Set background color with UIColor object
             view.backgroundColor = UIColor(red: red, green: green, blue: blue)
             
-            //clears colorButton text
+            // Clears colorButton text
             colorButton.setTitle("", for: .normal)
             
         } // end if
     } // end motionEnded()
+    
+    // Creates random RGB numbers and use them to set a new background color when device screen is tapped
+    @IBAction func handleTapGesture(_ sender: UITapGestureRecognizer) {
+        
+        if sender.state == .ended { // When screen is tapped
+            
+            // Create random RGB numbers
+            red = Int.random(in: 0...255)
+            green = Int.random(in: 0...255)
+            blue = Int.random(in: 0...255)
+            
+            // Set background color with UIColor object
+            view.backgroundColor = UIColor(red: red, green: green, blue: blue)
+            
+            // Clears colorButton text
+            colorButton.setTitle("", for: .normal)
+        } // end if
+        
+    } // end handleTapGesture()
     
 } // end ViewController
